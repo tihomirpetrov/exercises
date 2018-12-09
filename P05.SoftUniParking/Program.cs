a636bf725503547b7226ff7f4d25f8ae3010aebe
tihomirpetrov <tihomirpetrov79@gmail.com>
2018-12-10 00:47:55 +0200

    Dictionaries, Lambda and LINQ - Exercise

---
+++ P05.SoftUniParking/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace P05.SoftUniParking
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    class Program
+    {
+        public static void Main()
+        {
+            int numberOfCommands = int.Parse(Console.ReadLine());
+            Dictionary<string, string> registeredUser = new Dictionary<string, string>();
+
+            for (int i = 0; i < numberOfCommands; i++)
+            {
+                string[] input = Console.ReadLine().Split().ToArray();
+                string command = input[0];
+                string username = input[1];
+
+                if (command == "register")
+                {
+                    string licensePlateNumber = input[2];
+
+
+                    if (!registeredUser.ContainsKey(username))
+                    {
+                        registeredUser.Add(username, licensePlateNumber);
+                        Console.WriteLine($"{username} registered {licensePlateNumber} successfully");                        
+                    }
+                    else if (registeredUser[username].Contains(licensePlateNumber))
+                    {
+                        Console.WriteLine($"ERROR: already registered with plate number {licensePlateNumber}");
+                    }
+                    registeredUser[username] = licensePlateNumber;
+                }
+                else
+                {
+                    if (!registeredUser.ContainsKey(username))
+                    {
+                        Console.WriteLine($"ERROR: user {username} not found");
+                    }
+                    else
+                    {
+                        registeredUser.Remove(username);
+                        Console.WriteLine($"{username} unregistered successfully");
+                    }
+                }
+            }
+            foreach (var item in registeredUser)
+            {
+                Console.WriteLine($"{item.Key} => {item.Value}");
+            }
+        }
+    }
+}
