9b9abece9b3deaff8bba4ef0a0ad449cfd80c9da
tihomirpetrov <tihomirpetrov79@gmail.com>
2018-12-18 00:00:30 +0200

    string and text processing

---
+++ P03.TreasureFinder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace P03.TreasureFinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sequence = Console.ReadLine().Split().Select(int.Parse).ToArray();
+            string command = Console.ReadLine();
+            string decryptedCharLine = string.Empty;
+            StringBuilder sb = new StringBuilder();
+            List<string> lines = new List<string>();
+
+            while (command != "find")
+            {
+                char[] tokens = command.ToCharArray();
+
+                for (int i = 0; i < tokens.Length; i++)
+                {
+                    sb.Append(tokens[i] - (sequence[i]));
+                    //decryptedCharLine += (char.Parse(tokens[i])) - sequence[i];
+
+                }
+
+
+                command = Console.ReadLine();
+            }
+            Console.WriteLine(sb);
+        }
+    }
+}
