f3c3a03ce2dc8114ae7407b864bae643bfad600d
tihomirpetrov <tihomirpetrov79@gmail.com>
2018-12-17 23:15:38 +0200

    string and text processing

---
+++ P02.AsciiSumator/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace P02.AsciiSumator
+{
+    using System;
+
+    class Program
+    {
+        public static void Main()
+        {
+            char firstLineChar = char.Parse(Console.ReadLine());
+            char secondLineChar = char.Parse(Console.ReadLine());
+            string lastLine = Console.ReadLine();
+            int sum = 0;
+
+            if ((int)firstLineChar < (int)secondLineChar)
+            {
+                for (int i = 0; i < lastLine.Length; i++)
+                {
+                    if ((lastLine[i]) > (int)(firstLineChar) && (lastLine[i]) < (int)(secondLineChar))
+                    {
+                        sum += (int)(lastLine[i]);
+                    }
+                }
+            }            
+
+            Console.WriteLine(sum);
+        }
+    }
+}
